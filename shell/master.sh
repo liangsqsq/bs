14a768e8fb31fdd9717bd4ec58413c13eb71f13a
musJinx <musJinx18@outlook.com>
2023-04-28 19:29:57 +0800

    save

---
+++ shell/master.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+sudo apt-get update 
+sudo apt-get upgrade -y
+sudo apt-get install git -y
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source .bashrc
+echo 'pyenv安装完成，开始安装 python...'
+
+sudo apt-get install qemu-kvm -y
+sudo apt-get install qemu -y
+sudo apt-get install virt-manager -y
+sudo apt-get install virt-viewer -y
+sudo apt-get install libvirt-bin -y
+sudo apt-get install bridge-utils -y
+sudo apt-get install libvirt-dev -y
+sudo apt-get install libguestfs-tools -y
+sudo apt-get install gcc python-dev libguestfs-dev -y
+sudo apt-get install qemu-guest-agent -y
+sudo apt install \
+    build-essential \
+    curl \
+    libbz2-dev \
+    libffi-dev \
+    liblzma-dev \
+    libncursesw5-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    llvm \
+    make \
+    tk-dev \
+    wget \
+    xz-utils \
+    zlib1g-dev
+
+pyenv global 3.6.8
+pip install virtualenv -i https://pypi.python.org/simple/
--- shell/node.sh
+++ shell/node.sh
@@ -1,11 +1,15 @@
-#!/bin/bush
 sudo apt-get update
-sudo apt-get upgrade
-
+sudo apt-get upgrade -y
 wget  https://repo.zabbix.com/zabbix/4.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_4.0-3+xenial_all.deb
 sudo dpkg -i zabbix-release_4.0-3+xenial_all.deb
 sudo apt update
-
-sudo apt install qemu-kvm qemu virt-manager virt-viewer libvirt-bin bridge-utils
-sudo apt install libvirt-dev libguestfs-tools python-dev libguestfs-dev gcc
-sudo apt install vim openssh-server
+sudo apt install qemu-kvm qemu virt-manager virt-viewer libvirt-bin bridge-utils -y
+sudo apt install libvirt-dev libguestfs-tools python-dev libguestfs-dev gcc -y
+sudo apt install vim openssh-server -y
+echo '安装完成，开始修改配置文件'
+echo '创建资源池'
+virsh pool-define-as default dir --target /var/lib/libvirt/images/
+virsh pool-build default
+virsh pool-start default
+virsh pool-autostart default
+virsh pool-refresh default
